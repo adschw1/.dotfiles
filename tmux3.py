70a0012bc554bf70f182dcdd0f49e0969cf3dc04
Andrew <schwenk.andrew@yahoo.com>
2019-02-07 18:11:17 -0600

    Fixed issue where suffix would not apply to first window.

--- tmux3.py
+++ tmux3.py
@@ -7,9 +7,11 @@ suffix = int(raw_input("Starting # of suffix: "))
 first_prefix = str(prefix)
 command = ["tmux","new-session","-d","-s",session_name,"-n",first_prefix]
 subprocess.call(command)
+subprocess.call(["tmux","renamew","-t",first_prefix,(str(prefix)+str(suffix))])
 for i in range(2,windows+1):
-    prefix_ = str(prefix) + str(suffix)
-    subprocess.call(["tmux","neww","-n",prefix_])
     suffix+=1
+    prefix_ = (str(prefix) + str(suffix))
+    subprocess.call(["tmux","neww","-n",prefix_])
+
 
 subprocess.call(["tmux","attach","-t",session_name])
