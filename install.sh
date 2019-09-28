a4c45384cbf6598921206f244b51f0d56d613c06
Andrew Schwenk <SchwenkAndrew@JohnDeere.com>
2019-09-28 11:54:03 -0500

    improvments

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-DOTFILES="$HOME/.dotfiles"
-CURRENT_USER=$USER
+dotfiles="$home/.dotfiles"
+current_user=$user
 
 # to error out
 warn() {
@@ -18,39 +18,48 @@ lnif() {
     fi
 }
 
-echo "Installing/Updating DOTFILES...\n"
+echo "installing/updating dotfiles...\n"
 
-if [ ! -e $DOTFILES/.git ]; then
-    echo "Cloning DOTFILES\n"
-    # git clone ----repohere----
+if [ ! -e $dotfiles/.git ]; then
+    echo "cloning dotfiles\n"
+    git clone https://github.com/adschw1/.dotfiles.git
 else
-    echo "Updating DOTFILES\n"
-    cd $DOTFILES && git pull
+    echo "updating dotfiles\n"
+    cd $dotfiles && git pull
 fi
 
 # bash
-# echo "Setting up bash...\n"
+echo "setting up bash...\n"
+sudo -u $CURRENT_USER "lnif $dotfiles/.bashrc $home/.bashrc"
+sudo -u $CURRENT_USER "lnif $DOTFILES/.bash_profile $HOME/.bash_profile"
+sudo ln -s $DOTFILES/.bashrc /var/root/.bashrc
+sudo ln -s $DOTFILES/.bash_profile /var/root/.bash_profile
+
 
 # vim
 echo "Setting up vim...\n"
-lnif $DOTFILES/.vimrc $HOME/.vimrc
-lnif $DOTFILES/.vim_runtime $HOME/.vim_runtime
-sudo ln -s $HOME/.vimrc /var/root/.vimrc
-sudo ln -s $HOME/.vim_runtime /var/root/.vim_runtime
+sudo -u $CURRENT_USER "lnif $DOTFILES/.vimrc $HOME/.vimrc"
+sudo -u $CURRENT_USER "lnif $DOTFILES/.vim_runtime $HOME/.vim_runtime"
+sudo ln -s $DOTFILES/.vimrc /var/root/.vimrc
+sudo ln -s $DOTFILES/.vim_runtime /var/root/.vim_runtime
 
 # tmux
 echo "Setting up tmux...\n"
+sudo -u $CURRENT_USER "brew install tmux"
 lnif $DOTFILES/.tmux.conf $HOME/.tmux.conf
+sudo ln -s $DOTFILES/.tmux.conf /var/root/.tmux.conf
 
 # zsh
 echo "Setting up zsh...\n"
-sudo -u $CURRENT_USER brew install zsh
+sudo -u $CURRENT_USER "brew install zsh"
 
 # oh-my-zsh
 echo "Setting up oh-my-zsh"
+sudo -u $CURRENT_USER "sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)""
 chsh -s /bin/zsh
 sudo chsh -s /bin/zsh
-lnif $DOTFILES/.oh-my-zsh $HOME/.oh-my-zsh
-lnif $DOTFILES/.zshrc $HOME/.zshrc
-sudo ln -s $HOME/.oh-my-zsh /var/root/.oh-my-zsh
-sudo ln -s $HOME/.zshrc /var/root/.zshrc
+sudo -u $CURRENT_USER "lnif $DOTFILES/.oh-my-zsh $HOME/.oh-my-zsh"
+sudo -u $CURRENT_USER "lnif $DOTFILES/.zshrc $HOME/.zshrc"
+sudo ln -s $DOTFILES/.oh-my-zsh /var/root/.oh-my-zsh
+sudo ln -s $DOTFILES/.zshrc /var/root/.zshrc
+
